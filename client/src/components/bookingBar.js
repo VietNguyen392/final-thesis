5c6fcaf59bb370556109662c86ceacf2e54b61b4
VietNguyen392 <nghoangviet2000@hotmail.com>
2022-09-22 12:00:59 +0700

    push

--- client/src/components/bookingBar.js
+++ client/src/components/bookingBar.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { DatePicker, Row, Input, Col, Card, Select, Button } from 'antd';
+import {
+  DatePicker,
+  Row,
+  Input,
+  Col,
+  Card,
+  Select,
+  Button,
+  Dropdown,
+  Menu,
+} from 'antd';
 import {
   GlobalOutlined,
   UserOutlined,
   SearchOutlined,
+  CalendarOutlined,
+  GoldOutlined,
 } from '@ant-design/icons';
 
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 const BookingBar = () => {
+  const getDateValue = (ranges) => {
+    const valueOfInput1 = ranges[0].format();
+    const valueOfInput2 = ranges[1].format();
+  };
   return (
     <Card>
-      <Row gutter={8}>
+      <Row gutter={16}>
         <Col>
           <Input
             prefix={<GlobalOutlined />}
@@ -20,25 +36,35 @@ const BookingBar = () => {
           />
         </Col>
         <Col>
-          <RangePicker onChange={(e) => console.log(e.target.value)} />
+          <RangePicker onChange={getDateValue} />
+        </Col>
+
+        <Col>
+          <Input
+            type="number"
+            placeholder="Người lớn"
+            prefix={<UserOutlined />}
+            min="1"
+            style={{ width: '150px' }}
+          />
         </Col>
         <Col>
-          <Select style={{ width: 250 }} placeholder="Chọn">
-            <Option>
-              <Input
-                type="number"
-                prefix={<UserOutlined />}
-                placeholder="người lớn"
-              />
-            </Option>
-            <Option>
-              <Input
-                type="number"
-                prefix={<UserOutlined />}
-                placeholder="trẻ nhỏ"
-              />
-            </Option>
-          </Select>
+          <Input
+            type="number"
+            placeholder="Trẻ nhỏ"
+            prefix={<UserOutlined />}
+            min="1"
+            style={{ width: '150px' }}
+          />
+        </Col>
+        <Col>
+          <Input
+            type="number"
+            placeholder="Phòng"
+            prefix={<GoldOutlined />}
+            min="1"
+            style={{ width: '120px' }}
+          />
         </Col>
         <Col>
           <Button type="primary" icon={<SearchOutlined />}>
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,26 +1,22 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Banner } from 'Components/common';
-import { Space, Col, Divider, Row, Card } from 'antd';
+import { Space, Col, Row } from 'antd';
 import { fakeData } from '../../mock';
 import BookingBar from 'Components/bookingBar';
-const { Meta } = Card;
+import ListItem from 'Components/List';
 const Home = () => {
-  const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
   return (
     <div>
       <Banner />
 
       <Space style={{ margin: '20px' }}>
-        <Row gutter={32}>
+        <Row gutter={12}>
           <BookingBar />
         </Row>
+      </Space>
+      <Space className="container">
+        <ListItem />
       </Space>
     </div>
   );
